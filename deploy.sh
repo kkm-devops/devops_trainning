c8de067b3ef827a8c9e7351282a6ef08a578aece
kranti <bikumillan123@gmail.com>
2022-04-23 12:18:32 +0530

    critical changes

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
 this file for the deployment of the java baed application .
+Here we are updating the deployment file
+we have reset this fine 
+Now we will directly commit this file from work area to commit 
+
