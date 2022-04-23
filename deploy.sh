3344a7afbc9e796001710b39afb33c730e31217e
kranti <bikumillan123@gmail.com>
2022-04-23 21:32:01 +0530

    Deploy file updated in dev branch

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ this file for the deployment of the java baed application .
 Here we are updating the deployment file
 we have reset this fine 
 Now we will directly commit this file from work area to commit 
+we are editing thi file in "Development" branch and we push from development branch 
 
